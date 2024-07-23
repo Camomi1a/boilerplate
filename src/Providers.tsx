2929808e65fa334faf5c1bd708ba679a61d428f0
l.martynova <l.martynova@team.edstein.ru>
2024-07-23 16:09:36 +0300

    add redux

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -3,11 +3,16 @@
 import { SessionProvider } from "next-auth/react";
 import {store} from "@/src/redux/store";
 import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
+
+const persistor = persistStore(store);
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return <SessionProvider >
     <Provider store={store}>
-      {children}
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
     </Provider>
   </SessionProvider>;
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,8 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist/es/constants";
+import { persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
 import loggerMiddleware from "./middleware";
 
 const devMiddleware = [
@@ -7,6 +11,9 @@ const devMiddleware = [
 
 const middleware = (getDefaultMiddleware: any) => getDefaultMiddleware({
         immutableCheck: false,
+    serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
         thunk: true
     }).concat(devMiddleware)
 
@@ -15,9 +22,20 @@ const reducer =
         combineReducers({
         })
 
+const persistConfig = {
+    key: "root",
+    storage,
+    stateReconciler: hardSet,
+    version: 1,
+    blacklist: ['counter'] ,// What you don't wanna to persist
+    whitelist: ['test', 'account']
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export const store = configureStore({
 
-    reducer,
+    reducer: persistedReducer,
     middleware,
     devTools: process.env.NODE_ENV !== 'production'
 })
